caa7e74ba30ff7f88477facfd3e3d535c9a584d4
manik-babu <mdmanikbabu48@gmail.com>
2026-01-17 15:56:31 +0600

    Understand parallel route

--- src/app/(practiceParaRoute)/layout.tsx
+++ src/app/(practiceParaRoute)/layout.tsx
@@ -1,12 +1,61 @@
+import { Route } from "@/types";
 import Link from "next/link";
 
 export default function practiceParaRoute({ children, chatAndSettings, chatList }: { children: React.ReactNode; chatAndSettings: React.ReactNode; chatList: React.ReactNode; }) {
+    const user = {
+        role: "USER"
+    }
+    let routes: Route[] = [];
+
+    if (user.role === 'ADMIN') {
+        routes = [
+            {
+                title: "Admin Dashboard",
+                items: {
+                    title: 'Dashboard',
+                    url: '/dashboard'
+                }
+            },
+            {
+                title: "Manage user",
+                items: {
+                    title: 'Manage',
+                    url: '/manage'
+                }
+            }
+        ]
+    }
+    else {
+        routes = [
+            {
+                title: "User post",
+                items: {
+                    title: 'Post',
+                    url: '/post'
+                }
+            },
+            {
+                title: "users friend",
+                items: {
+                    title: 'Friend',
+                    url: '/friend'
+                }
+            }
+        ]
+    }
+
+
+
+
+
     return (
-        <div className="flex w-[100vw] h-[100vh] gap-2 bg-gray-200">
+        <div className="flex w-screen h-screen gap-2 bg-gray-200">
             <div className="flex flex-col gap-2">
-                <button><Link href='/people'>People</Link></button>
-                <button><Link href='/group'>Group</Link></button>
-                <button><Link href='/settings'>settings</Link></button>
+                {
+                    routes.map((route, index) => (
+                        <button key={index}><Link href={route.items.url}>{route.items.title}</Link></button>
+                    ))
+                }
             </div>
             {chatList}
             {chatAndSettings}
